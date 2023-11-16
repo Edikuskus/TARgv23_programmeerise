1244dcb69982ecfde13fbbe71208ee0dc9a318e0
Eduard <opilane@HP-CZC2349HT0>
2023-11-16 16:42:32 +0200

    Eduardd

---
+++ Work2/Work2.py
@@ -0,0 +1,28 @@
+﻿from math import *
+print("Ruudu karakteristikud")
+a=int(input("Sisesta ruudu külje pikkus => "))
+S=a**2
+print("Ruudu pindala", S)
+P=4*a
+print((input"Ruudu ümbermõõt", P))
+di=a*math.sqr(2)
+print((input"Ruudu diagonaal", round(di,2))
+print()
+print((input"Ristküliku karakteristikud"))
+b=input("Sisesta ristküliku 1. külje pikkus => ")
+c=input("Sisesta ristküliku 2. külje pikkus => ")
+S=b*c
+print("Ristküliku pindala", S)
+P=2(b+c)
+print("Ristküliku ümbermõõt", P)
+di=math.sqrt(b*2+c*2)
+print("Ristküliku diagonaal", round(di))
+print()
+print("Ringi karakteristikud")
+r=input("Sisesta ringi raadiusi pikkus => ")
+d=2*r
+print("Ringi läbimõõt", d)
+S=pi()*r*2
+print("Ringi pindala", round(S))
+C=2*pi()*r
+print("Ringjoone pikkus", round(C))
